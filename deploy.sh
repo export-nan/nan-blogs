714d1162282f2d771d79b74928638ae8abde705a
NAN <1066726487@qq.com>
2023-08-18 22:03:36 +0800

    修改部署规则

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,9 @@
-docker system prune -f
-
 docker stop nan-blogs
 
 docker rm nan-blogs
 
 docker rmi nan-blogs
 
-docker build -t nan-blogs .
+docker pull nan47/nan-blogs
 
-docker run -p 80:80 -d --name nan-blogs nan-blogs
+docker run -p 8080:80 -d --name nan-blogs nan47/nan-blogs
