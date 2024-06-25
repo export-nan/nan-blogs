959b0b2ccc26055b2af4bb12da161538610061d6
open-nan <54296915+open-nan@users.noreply.github.com>
2024-06-25 09:39:47 +0000

    Deploying to gh-pages from @ export-nan/nan-blogs@48658e9bb04370458e0f6d761a2c3a7251a0dcfb 🚀

--- assets/variable.html-DocqlJLo.js
+++
@@ -1,8 +0,0 @@
-import{_ as n}from"./plugin-vue_export-helper-x3n3nnut.js";import{o as a,c as s,f as e}from"./app-qc_LvvT5.js";const i={},t=e(`<h1 id="变量声明与数据类型" tabindex="-1"><a class="header-anchor" href="#变量声明与数据类型" aria-hidden="true">#</a> 变量声明与数据类型</h1><h2 id="声明变量" tabindex="-1"><a class="header-anchor" href="#声明变量" aria-hidden="true">#</a> 声明变量</h2><p>在<code>go</code>中我们使用<code>var</code>关键字声明变量</p><div class="language-go line-numbers-mode" data-ext="go"><pre class="language-go"><code><span class="token comment">// 使用var声明一个int类型的变量</span>
-<span class="token keyword">var</span> a <span class="token builtin">int</span>
-<span class="token comment">// 使用var声明一个int类型的变量，并赋值</span>
-<span class="token keyword">var</span> b <span class="token builtin">int</span> <span class="token operator">=</span> <span class="token number">0</span>
-<span class="token comment">// 使用var同时声明多个变量</span>
-<span class="token keyword">var</span> a <span class="token builtin">int</span><span class="token punctuation">,</span>b <span class="token builtin">int</span>
-<span class="token comment">// </span>
-</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div>`,4),c=[t];function o(l,r){return a(),s("div",null,c)}const v=n(i,[["render",o],["__file","variable.html.vue"]]);export{v as default};
